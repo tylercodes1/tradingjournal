c585613acb276bc9fcc20e7340b922ae7c5ebf13
Tyler Kim <49599494+tylercodes1@users.noreply.github.com>
2023-05-04 13:04:57 -0700

    ANALYTICS-1: add google analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,6 @@ export default function Home({ test }: test) {
 						Get started by editing&nbsp;
 						<code className={styles.code}>src/pages/index.tsx</code>
 					</p>
-					<div>{test}</div>
 					<div>
 						<a
 							href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
